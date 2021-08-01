cc34d30806a02c276f7ec92a37c0dd04d17f9cb5
nkmrkz <kazutaka.nakamura@ncdc.co.jp>
2021-08-02 02:49:56 +0900

    ワイルドカードドメインの検証

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,17 @@ const main = async () => {
     res.send("サブドメイン：" + subDomain[1]);
   });
 
+  app.get("/wildcard", (req, res) => {
+    console.log("GET /");
+    // const subDomain = req.headers.host
+    //   ? req.headers.host.match(/(.*)\.localhost\.com/)
+    //   : undefined;
+    // if (!subDomain) throw new Error();
+    // console.log("subDomain: ", subDomain[1]);
+    console.log("END /");
+    res.send(req.headers.host);
+  });
+
   // サーバ起動
   app.listen(3000, () => console.log(`app running`));
 };
